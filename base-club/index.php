14d98544411fe6d3333f83db8ef6ef4c72c99a15
deadpoetxxx <tasroses@gmail.com>
2025-07-16 14:10:38 +0300

    Update index.php

--- base-club/index.php
+++ base-club/index.php
@@ -1,1 +1,9 @@
+<?php
+echo "<h1>Καλωσήρθατε στον Σύλλογο!</h1>";
+$info = __DIR__ . '/info.txt';
+if (file_exists($info)) {
+    echo "<pre>" . file_get_contents($info) . "</pre>";
+} else {
+    echo "<p>Δεν υπάρχουν πληροφορίες.</p>";
+}
 
